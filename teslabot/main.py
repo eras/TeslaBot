4dc2eceeddae5a7ceed9c9f9e7478c2ce3a900be
Erkki Seppälä <erkki.seppala@vincit.fi>
2022-03-07 19:42:46 +0200

    matrix: do log-in

--- teslabot/main.py
+++ teslabot/main.py
@@ -1,12 +1,14 @@
+import asyncio
+
 from . import matrix
 from . import config
 
-async def async_main():
+async def async_main() -> None:
     args = config.get_args()
-    control = matrix.MatrixControl()
     config_ = config.Config(filename=args.config)
+    control = matrix.MatrixControl(config=config_)
     await control.setup()
     await control.run()
 
-def main():
+def main() -> None:
     asyncio.get_event_loop().run_until_complete(async_main())
--- teslabot/matrix.py
+++ teslabot/matrix.py
@@ -1,21 +1,57 @@
 import asyncio
 import re
 
+from dataclasses import dataclass
 from nio import AsyncClient, MatrixRoom, RoomMessageText
+from nio.responses import LoginResponse
+from configparser import ConfigParser
+from typing import Optional
 
 from . import control
+from .config import Config, ConfigElement
+
+class ConfigSave(ConfigElement):
+    control: "MatrixControl"
+
+    def __init__(self, control: "MatrixControl") -> None:
+        self.control = control
+
+    def save(self, config: ConfigParser) -> None:
+        if self.control.login_response:
+            config["matrix"]["mxid"]         = self.control.login_response.user_id
+            config["matrix"]["device_id"]    = self.control.login_response.device_id
+            config["matrix"]["access_token"] = self.control.login_response.access_token
 
 class MatrixControl(control.Control):
     client: AsyncClient
     room_id: str
+    config: Config
+    login_response: Optional[LoginResponse]
 
-    def __init__(self) -> None:
+    def __init__(self, config: Config) -> None:
         super().__init__()
-        self.client = AsyncClient("https://matrix.example.org", "@alice:example.org")
+        self.config = config
+        self.config.add_element(ConfigSave(self))
+        self.client = AsyncClient(self.config.config["matrix"]["homeserver"],
+                                  self.config.config["matrix"]["mxid"])
         self.client.add_event_callback(self._message_callback, RoomMessageText)
+        self.login_response = None
 
     async def setup(self) -> None:
-        await self.client.login("my-secret-password")
+        mx_config = self.config.config["matrix"]
+        if "matrix" in self.config.config and "access_token" in self.config.config["matrix"]:
+            logger.debug(f"Using pre-existing credentials")
+            self.client.user_id      = mx_config["mxid"]
+            self.client.device_id    = mx_config["device_id"]
+            self.client.access_token = mx_config["access_token"]
+        else:
+            logger.debug(f"Logging in")
+            login = await self.client.login(self.config.config["matrix"]["password"])
+
+            # "homeserver": homeserver,  # e.g. "https://matrix.example.org"
+            # "mxid": resp.user_id,  # e.g. "@user:example.org"
+            # "device_id": resp.device_id,  # device ID, 10 uppercase letters
+            # "access_token": resp.access_token  # cryptogr. access token
 
     async def send_message(self, message: str) -> None:
         await self.client.room_send(
