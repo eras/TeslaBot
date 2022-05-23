ae0d2bea867c94fdb4dec4126cdbe436b4134c8c
Niko Kangasniemi <niko.kangasniemi@vincit.fi>
2022-05-23 16:02:15 +0300

    typo

--- teslabot/commands.py
+++ teslabot/commands.py
@@ -47,8 +47,9 @@ class Invocation:
     def parse(message: str) -> "Invocation":
         fields = re.split(r"  *", message)
         if len(fields):
-            if fields[-1].startswith('<') and fields[-1].endswith('<'):
+            if fields[-1].startswith('<') and fields[-1].endswith('>'):
                 fields[-1] = fields[-1][1:-1]
+            logger.debug(f"Command: {fields}")
             return Invocation(name=fields[0],
                               args=fields[1:])
         else:
